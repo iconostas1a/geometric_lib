08f62b370eb865292e7fd9644a6fc5e01a88b9aa
Anastasia Melnikova <145349825+iconostas1a@users.noreply.github.com>
2023-11-14 20:19:44 +0300

    tests for triangle.py

--- triangle.py
+++ triangle.py
@@ -15,3 +15,16 @@ def perimeter(a, b, c):
 
     return a * h / 2
 
+
+
+class TriangleTestCase(unittest.TestCase):
+    def test_zero_mul_area(self):
+        self.assertEqual(area(10, 0), 0)
+
+    def test_negative_number(self):
+        self.assertEqual(area(-1, 8), "Incorrect input")
+
+        self.assertEqual(perimeter(10, -1, 8), "Incorrect input")
+
+    def test_zero_sum(self):
+        self.assertEqual(perimeter(8, 0, 9), 17)
