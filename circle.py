33b2a767ac844e0381f9a8787259bc15db1ade35
Anastasia Melnikova <145349825+iconostas1a@users.noreply.github.com>
2023-11-14 20:09:21 +0300

    tests for rectangle.py

--- circle.py
+++ circle.py
@@ -18,3 +18,17 @@ def perimeter(r):
 ```
     return 2 * math.pi * r
 
+
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_mul_area(self):
+        self.assertEqual(area(10, 0), 0)
+
+    def test_negative_number(self):
+        self.assertEqual(area(-1, 0), "Incorrect input")
+
+        self.assertEqual(perimeter(-5, -5), "Incorrect input")
+
+    def test_zero_sum(self):
+        self.assertEqual(perimeter(12, 0), 24)
+
+
