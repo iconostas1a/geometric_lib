30cae76e4ef912405865c6f033ad2a8d944542db
Anastasia Melnikova <145349825+iconostas1a@users.noreply.github.com>
2023-11-14 20:13:58 +0300

    tests for square.py

--- square.py
+++ square.py
@@ -14,3 +14,16 @@ def perimeter(a):
 возвращает а умноженное на 4
 ```
     return 4 * a
+
+
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_mul_area(self):
+        self.assertEqual(area(10, 0), 0)
+
+    def test_negative_number(self):
+        self.assertEqual(area(-1, 0), "Incorrect input")
+
+        self.assertEqual(perimeter(-5, -5), "Incorrect input")
+
+    def test_zero_sum(self):
+        self.assertEqual(perimeter(12, 0), 24)
